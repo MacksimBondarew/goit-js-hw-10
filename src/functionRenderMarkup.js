b188ad3ef07a888d4b734645cbc96c4b3e3f422d
MacksimBondarew <112969071+MacksimBondarew@users.noreply.github.com>
2023-02-19 22:42:42 +0200

    pls help

---
+++ src/functionRenderMarkup.js
@@ -0,0 +1,28 @@
+function createListCardCountrys(countrys) {
+    clearAllText();
+    const countrysList = countrys.map(country => {
+        return `<li class="country-list--item">
+            <img src="${country.flags.svg}" alt="Country flag" width="40", height="30">
+            <span class="country-list--name">${country.name.official}</span>
+        </li>`
+    }).join('');
+    resultListCountrys.insertAdjacentHTML('beforeend', countrysList);
+};
+
+function createOneCardCountry(country) {
+    clearAllText();
+    const oneCountry = country[0];
+    const markup = `
+    <div class="country-card--header">
+        <img src="${oneCountry.flags.svg}" alt="Country flag" width="55", height="35">
+        <h2 class="country-card--name"> ${oneCountry.name.official}</h2>
+    </div>
+        <p class="country-card--field">Capital: <span class="country-value">${oneCountry.capital}</span></p>
+        <p class="country-card--field">Population: <span class="country-value">${oneCountry.population}</span></p>
+        <p class="country-card--field">Languages: <span class="country-value">${Object.values(oneCountry.languages).join(',')}</span></p>
+    </div>`;
+
+    resultOneCountry.innerHTML = markup;
+};
+
+export { createOneCardCountry, createListCardCountrys};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
+//Розмітку html і стилі css взяв у людини яка це завдання вже робила ( вибачте що я такий лінивий :3 )
+import Notifix from 'notiflix';
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { createListCardCountrys, createOneCardCountry } from './functionRenderMarkup';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
+const inputSearch = document.querySelector("#search-box");
+const resultOneCountry = document.querySelector(".country-info");
+const resultListCountrys = document.querySelector('.country-list');
+
+inputSearch.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry() {
+    //Метод trim() удаляет пробельные символы с начала и конца строки. Пример:  |  Ukrain = |Ukrain; | - початок інпуту
+    let valueinputSearch = inputSearch.value.trim();
+    if (valueinputSearch === '') {
+        clearAllText();
+        return;
+    } else {
+        fetchCountries(valueinputSearch).then((countryName) => {
+            if (countryName.length < 2) {
+                createOneCardCountry(countryName);
+                Notifix.Notify.success('Here your result');
+            } else if (countryName.length > 1 && countryName.length < 10) {
+                createListCardCountrys(countryName);
+                Notifix.Notify.success('Here your result');
+            } else {
+                Notifix.Notify.info('Too many matches found. Please enter a more specific name.');
+            }
+        }).catch(() => { 
+            clearAllText();
+            Notifix.Notify.failure('Oops, there is no country with that name')});
+    }
+};
+
+//Щоб прибрати результат який був перед цим
+function clearAllText() {
+    resultListCountrys.innerHTML = '';
+    resultOneCountry.innerHTML = '';
+};
+
+// function createOneCardCountry(country) {
+//     clearAllText();
+//     const oneCountry = country[0];
+//     const markup = `
+//     <div class="country-card--header">
+//         <img src="${oneCountry.flags.svg}" alt="Country flag" width="55", height="35">
+//         <h2 class="country-card--name"> ${oneCountry.name.official}</h2>
+//     </div>
+//         <p class="country-card--field">Capital: <span class="country-value">${oneCountry.capital}</span></p>
+//         <p class="country-card--field">Population: <span class="country-value">${oneCountry.population}</span></p>
+//         <p class="country-card--field">Languages: <span class="country-value">${Object.values(oneCountry.languages).join(',')}</span></p>
+//     </div>`;
+
+//     resultOneCountry.innerHTML = markup;
+// }
+
+// function createListCardCountrys(countrys) {
+//     clearAllText();
+//     const countrysList = countrys.map(country => {
+//         return `<li class="country-list--item">
+//             <img src="${country.flags.svg}" alt="Country flag" width="40", height="30">
+//             <span class="country-list--name">${country.name.official}</span>
+//         </li>`
+//     }).join('');
+//     resultListCountrys.insertAdjacentHTML('beforeend', countrysList);
+// };
+
+
+
